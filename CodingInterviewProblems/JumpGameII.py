57fa2f383a03ba5088dd1cc5c1785839bac4cae1
sushant097 <sushant1997gautam@gmail.com>
2024-02-02 10:05:16 -0600

    Jump Game II solution added

---
+++ CodingInterviewProblems/JumpGameII.py
@@ -0,0 +1,52 @@
+"""
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+It's guaranteed that you can reach nums[n - 1].
+
+Leetcode: https://leetcode.com/problems/jump-game-ii/
+
+"""
+
+
+class Solution(object):
+    # Time: O(n) | Space: O(1)
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        currentEnd = 0
+        farthest = 0
+        jump = 0
+        for i in range(len(nums) - 1):# We do not need to check last element
+            farthest = max(farthest, i + nums[i])
+            if currentEnd == i:
+                currentEnd = farthest
+                jump += 1
+                if currentEnd >= len(nums) - 1:
+                    break
+        return jump
+            
